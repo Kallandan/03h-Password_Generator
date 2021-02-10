274152676cee53d6241c67403cb318341caa0e59
Addison Poole <johnmccarter0@gmail.com>
2021-02-09 19:58:05 -0500

    Progress commit: intermediate work on JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// declare character arrays
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specials = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ",", "<", ".", ">", "/", "[", "]", "{", "}", ";", ":", "'"];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,8 +19,9 @@ function writePassword() {
 function generatePassword() {
   // ask user how many characters they want for the password
   // be sure to let them know min 8 chars, 128 max
-  let 
-var passLength = parseInt(prompt("How many characters would you like your password to be?"
+  
+
+  var passLength = parseInt(prompt("How many characters would you like your password to be?"
                 + "\nMinimum of 8 characters; Maximum of 128 characters."));
   if (passLength < 8) {
     console.log("test case 1");
@@ -32,45 +35,53 @@ var passLength = parseInt(prompt("How many characters would you like your passwo
     console.log("test case 3");
     alert("WARNING: Seriously? Use a number dude, come on.");
     generatePassword();
-  } else {
+  }
     console.log("test case main");
     // ask if user wants lowercase/uppercase/nums/special chars
-    var lowerChars = confirm("Would you like to use lowercase characters for the password?");
-    if (!lowerChars) {
+    //var lowerChars = confirm("Would you like to use lowercase characters for the password?");
+    var lowerChoice = wantLowers();
+    var upperChoice = wantUppers();
+    var numChoice = wantNums();
+    var specChoice = wantChars();
+
+    if (!lowerChoice && !upperChoice && !numChoice && !specChoice) {
       //var upperChars = '';
-      var lowerChoice = lowerChars;
-      console.log(lowerChoice);
-      wantUppers();
-    } else {
-      var lowerChoice = lowerChars;
-      console.log(lowerChoice);
-      wantUppers();
+      alert("You cannot seriously not want anything. It would not be secure, you know, being all spaces and all.");
+      generatePassword();
     }
-    if (lowerChoice) {
-      // going to 
+    for(var i = 0; i < passLength; i++)
+    {
+      
     }
-    
   }
+}
+
+function wantLowers() {
+  var lowerChars = confirm("Would you like to use lowercase characters for the password?");
+  var lowerChoice = lowerChars;
+  console.log(lowerChoice);
+  return lowerChoice;
 }
 
 function wantUppers() {
   var upperChars = confirm("Would you like to use uppercase characters for the password?");
   var upperChoice = upperChars;
   console.log(upperChoice);
-  wantNums();
+  return upperChoice;
 }
 
 function wantNums() {
   var numChars = confirm("Would you like to use number characters for the password?");
   var numChoice = numChars;
   console.log(numChoice);
-  wantChars();
+  return numChoice;
 }
 
 function wantChars() {
   var specChars = confirm("Would you like to use special characters for the password?");
   var specChoice = specChars;
   console.log(specChoice);
+  return specChoice;
 }
 
 // Add event listener to generate button
